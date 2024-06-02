4898de165433fd37874c1594365c5901049cea0f
iballidev <iballi.dev@gmail.com>
2024-06-02 20:08:27 +0100

    development

--- src/index.php
+++ src/index.php
@@ -296,9 +296,11 @@
                         <a href="#" class="block h-72 overflow-hidden">
                             <img class="rounded-t-lg" src="./assets/images/power-outage-looms-round-the-nation-after-power-grid-collapse.png" alt="Noteworthy technology acquisitions 2021" />
                         </a>
-                        <div class="p-5">
+                        <div class="p-5 text-left">
                             <a href="#">
-                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    Power outage looms round the nation after power grid collapse
+                                </h5>
                             </a>
                             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
                             <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
