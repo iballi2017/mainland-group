d50fbdaca612d57f2ad1cd530e2f843652ac88c8
iballidev <iballi.dev@gmail.com>
2024-06-11 01:04:39 +0100

    update

--- src/tag-search-result.php
+++ src/tag-search-result.php
@@ -8,7 +8,7 @@
 
 <!-- header opens -->
 <?php
-$routes = array("Home", "Blog");
+$routes = array("Home", "Tag: Power Grid");
 include_once("./components/header.php"); ?>
 <!-- header closes -->
 
@@ -18,12 +18,12 @@ include_once("./components/header.php"); ?>
         <div class="mg-main-container">
             <div class="grid grid-cols-12 gap-4">
                 <div class="col-span-12 md:col-span-8">
-                    <div class="grid grid-cols-12 gap-4">
-                        <div class="col-span-12 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
-                            <a href="#" class="block max-h-96 overflow-hidden">
+                    <ul class="grid grid-cols-12 gap-4">
+                        <li class="col-span-12 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
+                            <a href="#" class="block max-h-72 overflow-hidden">
                                 <img class="rounded-t-lg" src="https://images.pexels.com/photos/733856/pexels-photo-733856.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" class="w-full" />
                             </a>
-                            <div class="p-4 lg:p-6">
+                            <div class="p-5">
                                 <div class="flex items-center gap-8 text-sm mb-2">
                                     <span><i class="far fa-calendar me-2"></i>May 10, 2021</span>
                                     <span><i class="far fa-user me-2"></i>By Admin</span>
@@ -31,75 +31,64 @@ include_once("./components/header.php"); ?>
                                 <a href="#">
                                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
                                 </a>
-                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. consectetur adipiscing elit, Quis ipsum suspendisse Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore mag.
-                                    <br>
-                                    <br>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, amet id, ab reprehenderit voluptate, placeat earum ut unde sint quaerat perferendis aliquam vel officia deleniti. Atque sequi facere exercitationem reprehenderit, officiis ad optio, doloribus suscipit a hic, quam temporibus quod sit magni fuga voluptatem aliquam ipsam veritatis doloremque veniam qui.
-                                </p>
-
+                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. consectetur adipiscing elit, Quis ipsum suspendisse Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore mag...</p>
+                                <a href="#" class="focus:outline-none focus:ring-1 focus:ring-accent-50 | inline-flex bg-theme-clr-call-to-action hover:bg-primary-alt dark:hover:bg-black/15 text-black dark:hover:text-white dark:hover:border dark:hover:border-white font-semibold shadow-md rounded py-2 px-6 uppercase">
+                                    Read more
+                                </a>
                             </div>
-                        </div>
-
-                        <div class="col-span-12 block p-4 lg:p-6 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
-
-                            <h5 class="mb-6 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                                Tags
-                            </h5>
-
-                            <ul class="flex flex-wrap gap-2 text-xs">
-                                <li>
-                                    <a href="#" class="block text-nowrap p-2 bg-theme-clr-light hover:bg-theme-clr-light/50 rounded">Electricity</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="block text-nowrap p-2 bg-theme-clr-light hover:bg-theme-clr-light/50 rounded">Nationwide</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="block text-nowrap p-2 bg-theme-clr-light hover:bg-theme-clr-light/50 rounded">Power grid</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="block text-nowrap p-2 bg-theme-clr-light hover:bg-theme-clr-light/50 rounded">Collapse</a>
-                                </li>
-                            </ul>
-
-                        </div>
-                    </div>
-
-                    <div class="h-8"></div>
-                    <!-- Pagination -->
-                    <ul class="text-sm flex items-center justify-between gap-4">
-                        <li class="hidden lg:block">
-                            <a href="#">
-                                <span class="block font-semibold mb-4">Previous</span>
-                                <div class="flex items-center gap-2">
-                                    <div class="rounded-lg w-10 lg:w-12 overflow-hidden">
-                                        <img src="./assets/images/food-price-increases-by-200-percent-progression-say-it-may-increase.png" alt="">
-                                    </div>
-                                    <div class="text-on-theme-clr-light">
-                                        <p class="font-semibold">Fixing Solar panel of a client</p>
-                                    </div>
-                                </div>
-                            </a>
                         </li>
-                        <li>
-                            <a href="#">
-                                <span class="lg:text-right block font-semibold mb-4">Next</span>
-                                <div class="flex items-center gap-2">
-                                    <div class="rounded-lg w-10 lg:w-12 overflow-hidden">
-                                        <img src="./assets/images/food-price-increases-by-200-percent-progression-say-it-may-increase.png" alt="">
-                                    </div>
-                                    <div class="text-on-theme-clr-light">
-                                        <p class="font-semibold">Fuel Scarcity and hike in price may cause slow ...</p>
-                                    </div>
-                                </div>
+                        <li class="col-span-12 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
+                            <a href="#" class="block max-h-72 overflow-hidden">
+                                <img class="rounded-t-lg" src="https://images.pexels.com/photos/733856/pexels-photo-733856.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" class="w-full" />
                             </a>
+                            <div class="p-5">
+                                <div class="flex items-center gap-8 text-sm mb-2">
+                                    <span><i class="far fa-calendar me-2"></i>May 10, 2021</span>
+                                    <span><i class="far fa-user me-2"></i>By Admin</span>
+                                </div>
+                                <a href="#">
+                                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                                </a>
+                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. consectetur adipiscing elit, Quis ipsum suspendisse Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore mag...</p>
+                                <a href="#" class="focus:outline-none focus:ring-1 focus:ring-accent-50 | inline-flex bg-theme-clr-call-to-action hover:bg-primary-alt dark:hover:bg-black/15 text-black dark:hover:text-white dark:hover:border dark:hover:border-white font-semibold shadow-md rounded py-2 px-6 uppercase">
+                                    Read more
+                                </a>
+                            </div>
                         </li>
                     </ul>
+
+                    <div class="h-8"></div>
+                    <nav aria-label="Page navigation example">
+                        <ul class="inline-flex -space-x-px text-xs lg:text-sm gap-2">
+                            <li>
+                                <a href="#" class="flex items-center justify-center rounded px-3 h-8 ms-0 leading-tight text-on-theme-clr-white bg-white hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
+                            </li>
+                            <li>
+                                <a href="#" class="flex items-center justify-center rounded-full w-8 h-8 leading-tight text-gray-500 bg-white border border-theme-clr-secondary hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
+                            </li>
+                            <li>
+                                <a href="#" class="flex items-center justify-center rounded-full  w-8 h-8 leading-tight text-gray-500 bg-white border border-theme-clr-secondary hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
+                            </li>
+                            <li>
+                                <a href="#" aria-current="page" class="flex items-center justify-center rounded-full  w-8 h-8 text-on-theme-clr-secondary border border-theme-clr-secondary bg-theme-clr-secondary/50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
+                            </li>
+                            <li>
+                                <a href="#" class="flex items-center justify-center rounded-full  w-8 h-8 leading-tight text-gray-500 bg-white border border-theme-clr-secondary hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
+                            </li>
+                            <li>
+                                <a href="#" class="flex items-center justify-center rounded-full  w-8 h-8 leading-tight text-gray-500 bg-white border border-theme-clr-secondary hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
+                            </li>
+                            <li>
+                                <a href="#" class="flex items-center justify-center rounded px-3 h-8 leading-tight text-on-theme-clr-white bg-white hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
                 <div class="col-span-12 md:col-span-4">
                     <ul class="grid grid-cols-12 gap-4">
                         <li class="col-span-12">
 
-                            <div class="block p-4 lg:p-6 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
+                            <div class="block p-6 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
 
                                 <h5 class="mb-6 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
                                     Recent post
@@ -143,7 +132,7 @@ include_once("./components/header.php"); ?>
                         </li>
                         <li class="col-span-12">
 
-                            <div class="block p-4 lg:p-6 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
+                            <div class="block p-6 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
 
                                 <h5 class="mb-6 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
                                     Our tags
