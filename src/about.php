91183121c47307826b0da0d017603d58238bf777
iballidev <iballi.dev@gmail.com>
2024-06-22 01:09:10 +0100

    about page update

--- src/about.php
+++ src/about.php
@@ -45,10 +45,10 @@ include_once("./components/header.php"); ?>
     <section class="bg-theme-clr-light dark:text-on-theme-clr-light py-12 lg:py-20">
         <div class="mg-main-container">
             <div class="grid grid-cols-12 gap-8 md:gap-12">
-                <div class="col-span-12 md:col-start-1 md:col-end-9 bg-theme-clr-white p-4 lg:p-6 shadow-sm rounded-lg">
-                    <div class="flex flex-col items-start jusify-center gap-4">
-                        <h2 class="text-3xl font-semibold">Lagoscart </h2>
-                        <p class="text-lg">
+                <div class="col-span-12 md:col-start-1 md:col-end-9 bg-theme-clr-white p-4 lg:p-6 shadow-sm rounded-lg fade-in" data-delay="400">
+                    <div class="flex flex-col items-start jusify-center gap-2 md:gap-4">
+                        <h2 class="text-xl lg:text-3xl font-semibold">Lagoscart </h2>
+                        <p class="lg:text-lg">
                             Lagoscart is your trusted online shopping destination. We offer a wide variety of products including groceries, electronics, fashion, and home goods. Our platform is designed for convenience, ensuring you can shop from the comfort of your home and have your purchases delivered swiftly to your doorstep. At Lagoscart, customer satisfaction is our top priority, and we strive to provide a seamless shopping experience with competitive prices and exceptional service.
                         </p>
 
@@ -62,10 +62,10 @@ include_once("./components/header.php"); ?>
                     </div>
                 </div>
                 <!--  -->
-                <div class="col-span-12 md:col-start-4 md:col-end-13 bg-theme-clr-white p-4 lg:p-6 shadow-sm rounded-lg">
-                    <div class="flex flex-col items-start jusify-center gap-4">
-                        <h2 class="text-3xl font-semibold">Vanlagos </h2>
-                        <p class="text-lg">
+                <div class="col-span-12 md:col-start-4 md:col-end-13 bg-theme-clr-white p-4 lg:p-6 shadow-sm rounded-lg fade-in" data-delay="400">
+                    <div class="flex flex-col items-start jusify-center gap-2 md:gap-4">
+                        <h2 class="text-xl lg:text-3xl font-semibold">Vanlagos </h2>
+                        <p class="lg:text-lg">
                             Vanlagos.com is a reliable vehicle hiring company that provides premium services to our customers at an affordable and competitive market price. In the areas of safety, timeliness, efficiency and excellent customer service, Vanlagos provides outstanding offers for you. We also have tools in place to provide support and assist you in a situation which is why you should choose us.
                             <br />
                             Our rigorous vetting process has ensured that all our drivers are qualified and in compliance with all the regulations of applicable bodies. When you book a bus/van with us, your safety is assured as a result of the trust you have established with us to get you to your location.
@@ -82,10 +82,10 @@ include_once("./components/header.php"); ?>
                 </div>
                 <!--  -->
 
-                <div class="col-span-12 md:col-start-1 md:col-end-9 bg-theme-clr-white p-4 lg:p-6 shadow-sm rounded-lg">
-                    <div class="flex flex-col items-start jusify-center gap-4">
-                        <h2 class="text-3xl font-semibold">Mainland Solar</h2>
-                        <p class="text-lg">Mainlandsolar is dedicated to promoting sustainable energy solutions through advanced solar power technology. We offer comprehensive solar installation services for residential, commercial, and industrial clients. Our goal is to reduce carbon footprints and help you achieve energy independence with high-quality solar panels and expert installation. Trust Mainlandsolar to provide you with clean, efficient, and cost-effective energy solutions tailored to your needs.</p>
+                <div class="col-span-12 md:col-start-1 md:col-end-9 bg-theme-clr-white p-4 lg:p-6 shadow-sm rounded-lg fade-in" data-delay="400">
+                    <div class="flex flex-col items-start jusify-center gap-2 md:gap-4">
+                        <h2 class="text-xl lg:text-3xl font-semibold">Mainland Solar</h2>
+                        <p class="lg:text-lg">Mainlandsolar is dedicated to promoting sustainable energy solutions through advanced solar power technology. We offer comprehensive solar installation services for residential, commercial, and industrial clients. Our goal is to reduce carbon footprints and help you achieve energy independence with high-quality solar panels and expert installation. Trust Mainlandsolar to provide you with clean, efficient, and cost-effective energy solutions tailored to your needs.</p>
 
                         <div class="flex">
                             <?php
@@ -97,20 +97,20 @@ include_once("./components/header.php"); ?>
                     </div>
                 </div>
                 <!--  -->
-                <!-- <div class="col-span-4 bg-theme-clr-white p-4 lg:p-6 shadow-sm rounded-lg">
+                <!-- <div class="col-span-4 bg-theme-clr-white p-4 lg:p-6 shadow-sm rounded-lg fade-in" data-delay="400">
                     vv
                 </div> -->
 
                 <!-- <div class="col-span-12 md:col-start-4"> -->
-                <div class="col-span-12 bg-theme-clr-white p-4 lg:p-6 shadow-sm rounded-lg">
-                    <div class="flex flex-col items-start jusify-center gap-4">
-                        <h2 class="text-3xl font-semibold">Studios Mainland</h2>
+                <div class="col-span-12 bg-theme-clr-white p-4 lg:p-6 shadow-sm rounded-lg fade-in" data-delay="400">
+                    <div class="flex flex-col items-start jusify-center gap-2 md:gap-4">
+                        <h2 class="text-xl lg:text-3xl font-semibold">Studios Mainland</h2>
 
-                        <p class="text-lg">
+                        <p class="lg:text-lg">
                             At Studios Mainland, we are dedicated to being part of your story by making it memorable. We offer a wide range of services, including event coverage, video production, podcasts, video ads, and editing services. Our commitment to excellence and creativity sets us apart as one of the most reliable studio agencies in Lagos.
                         </p>
 
-                        <section class="">
+                        <section class="fade-in" data-delay="400">
                             <h3 class="text-2xl font-bold mb-4">Services Provided</h3>
                             <div class="grid grid-cols-12 gap-4 lg:gap-8">
                                 <div class="col-span-12 md:col-span-3">
